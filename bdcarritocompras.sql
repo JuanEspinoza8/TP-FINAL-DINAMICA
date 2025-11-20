e1f5863e149f2b65116f203c1d15ff9b07e86452
JuanEspinoza8 <juanespinoza.unco@gmail.com>
2025-11-19 23:50:34 -0300

    BD actualizada bien

---
+++ bdcarritocompras.sql
@@ -0,0 +1,447 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 20-11-2025 a las 03:48:43
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bdcarritocompras`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `compra`
+--
+
+CREATE TABLE `compra` (
+  `idcompra` bigint(20) NOT NULL,
+  `cofecha` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `idusuario` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `compra`
+--
+
+INSERT INTO `compra` (`idcompra`, `cofecha`, `idusuario`) VALUES
+(1, '2025-11-20 02:58:53', 1),
+(2, '2025-11-20 03:14:44', 2),
+(3, '2025-11-20 03:19:09', 2),
+(4, '2025-11-20 05:57:31', 1),
+(5, '2025-11-20 05:58:11', 1),
+(6, '2025-11-20 06:02:44', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `compraestado`
+--
+
+CREATE TABLE `compraestado` (
+  `idcompraestado` bigint(20) UNSIGNED NOT NULL,
+  `idcompra` bigint(11) NOT NULL,
+  `idcompraestadotipo` int(11) NOT NULL,
+  `cefechaini` timestamp NOT NULL DEFAULT current_timestamp(),
+  `cefechafin` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `compraestado`
+--
+
+INSERT INTO `compraestado` (`idcompraestado`, `idcompra`, `idcompraestadotipo`, `cefechaini`, `cefechafin`) VALUES
+(1, 1, 5, '2025-11-20 02:58:53', '2025-11-20 03:11:09'),
+(2, 1, 1, '2025-11-20 03:11:09', '2025-11-20 03:19:39'),
+(3, 2, 5, '2025-11-20 03:14:44', '2025-11-20 03:15:00'),
+(4, 2, 1, '2025-11-20 03:15:00', '2025-11-20 03:20:59'),
+(5, 3, 5, '2025-11-20 03:19:09', NULL),
+(6, 1, 2, '2025-11-20 03:19:39', '2025-11-20 03:20:22'),
+(7, 1, 3, '2025-11-20 03:20:22', NULL),
+(8, 2, 4, '2025-11-20 03:20:59', NULL),
+(9, 4, 5, '2025-11-20 05:57:31', '2025-11-20 05:57:38'),
+(10, 4, 1, '2025-11-20 05:57:38', '2025-11-20 05:57:47'),
+(11, 4, 2, '2025-11-20 05:57:47', '2025-11-20 05:57:57'),
+(12, 4, 3, '2025-11-20 05:57:57', NULL),
+(13, 5, 5, '2025-11-20 05:58:11', '2025-11-20 05:58:42'),
+(14, 5, 1, '2025-11-20 05:58:42', '2025-11-20 05:58:48'),
+(15, 5, 2, '2025-11-20 05:58:48', '2025-11-20 05:58:50'),
+(16, 5, 3, '2025-11-20 05:58:50', NULL),
+(17, 6, 5, '2025-11-20 06:02:44', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `compraestadotipo`
+--
+
+CREATE TABLE `compraestadotipo` (
+  `idcompraestadotipo` int(11) NOT NULL,
+  `cetdescripcion` varchar(50) NOT NULL,
+  `cetdetalle` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `compraestadotipo`
+--
+
+INSERT INTO `compraestadotipo` (`idcompraestadotipo`, `cetdescripcion`, `cetdetalle`) VALUES
+(1, 'iniciada', 'cuando el usuario : cliente inicia la compra de uno o mas productos del carrito'),
+(2, 'aceptada', 'cuando el usuario administrador da ingreso a uno de las compras en estado = 1 '),
+(3, 'enviada', 'cuando el usuario administrador envia a uno de las compras en estado =2 '),
+(4, 'cancelada', 'un usuario administrador podra cancelar una compra en cualquier estado y un usuario cliente solo en estado=1 '),
+(5, 'carrito', 'Estado temporal mientras el usuario carga productos');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `compraitem`
+--
+
+CREATE TABLE `compraitem` (
+  `idcompraitem` bigint(20) UNSIGNED NOT NULL,
+  `idproducto` bigint(20) NOT NULL,
+  `idcompra` bigint(20) NOT NULL,
+  `cicantidad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `compraitem`
+--
+
+INSERT INTO `compraitem` (`idcompraitem`, `idproducto`, `idcompra`, `cicantidad`) VALUES
+(1, 1, 1, 1),
+(2, 1, 2, 1),
+(4, 1, 4, 1),
+(6, 2, 5, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `menu`
+--
+
+CREATE TABLE `menu` (
+  `idmenu` bigint(20) NOT NULL,
+  `menombre` varchar(50) NOT NULL COMMENT 'Nombre del item del menu',
+  `medescripcion` varchar(124) NOT NULL COMMENT 'Descripcion mas detallada del item del menu',
+  `idpadre` bigint(20) DEFAULT NULL COMMENT 'Referencia al id del menu que es subitem',
+  `medeshabilitado` timestamp NULL DEFAULT current_timestamp() COMMENT 'Fecha en la que el menu fue deshabilitado por ultima vez'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `menu`
+--
+
+INSERT INTO `menu` (`idmenu`, `menombre`, `medescripcion`, `idpadre`, `medeshabilitado`) VALUES
+(1, 'Productos', '/proyecto/Vista/productos.php', NULL, NULL),
+(2, 'Mi Carrito', '/proyecto/Vista/carrito.php', NULL, NULL),
+(3, 'Mis Compras', '/proyecto/Vista/listaCompras.php', NULL, NULL),
+(4, 'Gestion Usuarios', '/proyecto/Vista/listaUsuarios.php', NULL, NULL),
+(5, 'Depósito', '/proyecto/Vista/gestionProductos.php', NULL, NULL),
+(6, 'Gestion Pedidos', '/proyecto/Vista/administrarCompras.php', NULL, NULL),
+(7, 'Gestion Menús', '/proyecto/Vista/gestionMenu.php', NULL, NULL),
+(8, 'Mi Perfil', '/proyecto/Vista/modificarPerfil.php', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `menurol`
+--
+
+CREATE TABLE `menurol` (
+  `idmenu` bigint(20) NOT NULL,
+  `idrol` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `menurol`
+--
+
+INSERT INTO `menurol` (`idmenu`, `idrol`) VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(2, 2),
+(3, 2),
+(4, 1),
+(5, 1),
+(5, 3),
+(6, 1),
+(6, 3),
+(7, 1),
+(8, 1),
+(8, 2),
+(8, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `idproducto` bigint(20) NOT NULL,
+  `pronombre` varchar(100) NOT NULL,
+  `prodetalle` varchar(512) NOT NULL,
+  `procantstock` int(11) NOT NULL,
+  `proprecio` decimal(10,2) NOT NULL DEFAULT 0.00,
+  `proimagen` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`idproducto`, `pronombre`, `prodetalle`, `procantstock`, `proprecio`, `proimagen`) VALUES
+(1, 'Memoria RAM', '16GB', 19, 1000.00, ''),
+(2, 'Monitor', 'ASUS 24\"', 0, 100000.00, 'https://mexx-img-2019.s3.amazonaws.com/Monitor-Gamer-27-Asus-Tuf-Gaming-Ips-180Hz-1Ms-VG279Q3A_48431_3.jpeg');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rol`
+--
+
+CREATE TABLE `rol` (
+  `idrol` bigint(20) NOT NULL,
+  `rodescripcion` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `rol`
+--
+
+INSERT INTO `rol` (`idrol`, `rodescripcion`) VALUES
+(1, 'Admin'),
+(2, 'Cliente'),
+(3, 'Deposito');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `idusuario` bigint(20) NOT NULL,
+  `usnombre` varchar(50) NOT NULL,
+  `uspass` varchar(255) NOT NULL,
+  `usmail` varchar(50) NOT NULL,
+  `usdeshabilitado` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`idusuario`, `usnombre`, `uspass`, `usmail`, `usdeshabilitado`) VALUES
+(1, 'Juanxiz', '36727ef12c80611118a7cabe1e451b22', 'starcraft.mj@gmail.com', NULL),
+(2, 'Juanxiz2', '4983a0ab83ed86e0e7213c8783940193', 'juancruz.espinoza@yahoo.com.ar', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuariorol`
+--
+
+CREATE TABLE `usuariorol` (
+  `idusuario` bigint(20) NOT NULL,
+  `idrol` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `usuariorol`
+--
+
+INSERT INTO `usuariorol` (`idusuario`, `idrol`) VALUES
+(1, 1),
+(2, 2);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `compra`
+--
+ALTER TABLE `compra`
+  ADD PRIMARY KEY (`idcompra`),
+  ADD UNIQUE KEY `idcompra` (`idcompra`),
+  ADD KEY `fkcompra_1` (`idusuario`);
+
+--
+-- Indices de la tabla `compraestado`
+--
+ALTER TABLE `compraestado`
+  ADD PRIMARY KEY (`idcompraestado`),
+  ADD UNIQUE KEY `idcompraestado` (`idcompraestado`),
+  ADD KEY `fkcompraestado_1` (`idcompra`),
+  ADD KEY `fkcompraestado_2` (`idcompraestadotipo`);
+
+--
+-- Indices de la tabla `compraestadotipo`
+--
+ALTER TABLE `compraestadotipo`
+  ADD PRIMARY KEY (`idcompraestadotipo`);
+
+--
+-- Indices de la tabla `compraitem`
+--
+ALTER TABLE `compraitem`
+  ADD PRIMARY KEY (`idcompraitem`),
+  ADD UNIQUE KEY `idcompraitem` (`idcompraitem`),
+  ADD KEY `fkcompraitem_1` (`idcompra`),
+  ADD KEY `fkcompraitem_2` (`idproducto`);
+
+--
+-- Indices de la tabla `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`idmenu`),
+  ADD UNIQUE KEY `idmenu` (`idmenu`),
+  ADD KEY `fkmenu_1` (`idpadre`);
+
+--
+-- Indices de la tabla `menurol`
+--
+ALTER TABLE `menurol`
+  ADD PRIMARY KEY (`idmenu`,`idrol`),
+  ADD KEY `fkmenurol_2` (`idrol`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`idproducto`),
+  ADD UNIQUE KEY `idproducto` (`idproducto`);
+
+--
+-- Indices de la tabla `rol`
+--
+ALTER TABLE `rol`
+  ADD PRIMARY KEY (`idrol`),
+  ADD UNIQUE KEY `idrol` (`idrol`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idusuario`),
+  ADD UNIQUE KEY `idusuario` (`idusuario`);
+
+--
+-- Indices de la tabla `usuariorol`
+--
+ALTER TABLE `usuariorol`
+  ADD PRIMARY KEY (`idusuario`,`idrol`),
+  ADD KEY `idusuario` (`idusuario`),
+  ADD KEY `idrol` (`idrol`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `compra`
+--
+ALTER TABLE `compra`
+  MODIFY `idcompra` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `compraestado`
+--
+ALTER TABLE `compraestado`
+  MODIFY `idcompraestado` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT de la tabla `compraitem`
+--
+ALTER TABLE `compraitem`
+  MODIFY `idcompraitem` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `idmenu` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `idproducto` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `rol`
+--
+ALTER TABLE `rol`
+  MODIFY `idrol` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `idusuario` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `compra`
+--
+ALTER TABLE `compra`
+  ADD CONSTRAINT `fkcompra_1` FOREIGN KEY (`idusuario`) REFERENCES `usuario` (`idusuario`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `compraestado`
+--
+ALTER TABLE `compraestado`
+  ADD CONSTRAINT `fkcompraestado_1` FOREIGN KEY (`idcompra`) REFERENCES `compra` (`idcompra`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `fkcompraestado_2` FOREIGN KEY (`idcompraestadotipo`) REFERENCES `compraestadotipo` (`idcompraestadotipo`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `compraitem`
+--
+ALTER TABLE `compraitem`
+  ADD CONSTRAINT `fkcompraitem_1` FOREIGN KEY (`idcompra`) REFERENCES `compra` (`idcompra`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `fkcompraitem_2` FOREIGN KEY (`idproducto`) REFERENCES `producto` (`idproducto`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `menu`
+--
+ALTER TABLE `menu`
+  ADD CONSTRAINT `fkmenu_1` FOREIGN KEY (`idpadre`) REFERENCES `menu` (`idmenu`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `menurol`
+--
+ALTER TABLE `menurol`
+  ADD CONSTRAINT `fkmenurol_1` FOREIGN KEY (`idmenu`) REFERENCES `menu` (`idmenu`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `fkmenurol_2` FOREIGN KEY (`idrol`) REFERENCES `rol` (`idrol`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `usuariorol`
+--
+ALTER TABLE `usuariorol`
+  ADD CONSTRAINT `fkmovimiento_1` FOREIGN KEY (`idrol`) REFERENCES `rol` (`idrol`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `usuariorol_ibfk_2` FOREIGN KEY (`idusuario`) REFERENCES `usuario` (`idusuario`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
